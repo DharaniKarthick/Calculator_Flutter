ad168c57391f9b09033c7706bbf4d303cd7da97e
Dharani_Karthick <dharanik305@gmail.com>
2024-06-19 14:58:49 +0000

    Calculator added

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String output = "0";
   String _output = "0";
+  String expression = "";
   double num1 = 0.0;
   double num2 = 0.0;
   String operand = "";
@@ -33,15 +34,24 @@ class _MyHomePageState extends State<MyHomePage> {
   buttonPressed(String buttonText) {
     if (buttonText == "CLEAR") {
       _output = "0";
+      expression = "";
       num1 = 0.0;
       num2 = 0.0;
       operand = "";
     } else if (buttonText == "+" ||
         buttonText == "-" ||
         buttonText == "/" ||
-        buttonText == "X") {
-      num1 = double.parse(output);
+        buttonText == "*") {
+      if (operand.isEmpty) {
+        num1 = double.parse(_output);
+      } else {
+        num2 = double.parse(_output);
+        _output = performOperation(num1, num2, operand).toString();
+        num1 = double.parse(_output);
+        num2 = 0.0;
+      }
       operand = buttonText;
+      //expression += buttonText;
       _output = "0";
     } else if (buttonText == ".") {
       if (_output.contains(".")) {
@@ -50,29 +60,43 @@ class _MyHomePageState extends State<MyHomePage> {
         _output = _output + buttonText;
       }
     } else if (buttonText == "=") {
-      num2 = double.parse(output);
-      if (operand == "+") {
-        _output = (num1 + num2).toString();
-      }
-      if (operand == "-") {
-        _output = (num1 - num2).toString();
-      }
-      if (operand == "X") {
-        _output = (num1 * num2).toString();
-      }
-      if (operand == "/") {
-        _output = (num1 / num2).toString();
-      }
-      num1 = 0.0;
-      num2 = 0.0;
+      num2 = double.parse(_output);
+      _output = performOperation(num1, num2, operand).toString();
+      expression = "";
       operand = "";
     } else {
-      _output = _output + buttonText;
+      if (_output == "0") {
+        _output = buttonText;
+      } else {
+        _output = _output + buttonText;
+      }
     }
 
     setState(() {
-      output = double.parse(_output).toStringAsFixed(2);
+      if (buttonText == "CLEAR") {
+        expression = "";
+      } else if (buttonText != "=") {
+        expression += buttonText;
+      } else {
+        expression = _output;
+      }
+      output = _output;
     });
+  }
+
+  double performOperation(double num1, double num2, String operand) {
+    switch (operand) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      case "*":
+        return num1 * num2;
+      case "/":
+        return num1 / num2;
+      default:
+        return 0.0;
+    }
   }
 
   Widget buildButton(String buttonText) {
@@ -99,55 +123,66 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
-              child: Text(
-                output,
-                style: TextStyle(
-                  fontSize: 48.0,
-                  fontWeight: FontWeight.bold,
-                ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              expression,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
               ),
             ),
-            Expanded(
-              child: Divider(),
+          ),
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              output,
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            Column(children: [
-              Row(children: [
-                buildButton("7"),
-                buildButton("8"),
-                buildButton("9"),
-                buildButton("/")
-              ]),
-              Row(children: [
-                buildButton("4"),
-                buildButton("5"),
-                buildButton("6"),
-                buildButton("X")
-              ]),
-              Row(children: [
-                buildButton("1"),
-                buildButton("2"),
-                buildButton("3"),
-                buildButton("-")
-              ]),
-              Row(children: [
-                buildButton("."),
-                buildButton("0"),
-                buildButton("00"),
-                buildButton("+")
-              ]),
-              Row(children: [
-                buildButton("CLEAR"),
-                buildButton("="),
-              ])
-            ])
-          ],
-        ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(children: [
+                  buildButton("7"),
+                  buildButton("8"),
+                  buildButton("9"),
+                  buildButton("/")
+                ]),
+                Row(children: [
+                  buildButton("4"),
+                  buildButton("5"),
+                  buildButton("6"),
+                  buildButton("*")
+                ]),
+                Row(children: [
+                  buildButton("1"),
+                  buildButton("2"),
+                  buildButton("3"),
+                  buildButton("-")
+                ]),
+                Row(children: [
+                  buildButton("."),
+                  buildButton("0"),
+                  buildButton("00"),
+                  buildButton("+")
+                ]),
+                Row(children: [
+                  buildButton("CLEAR"),
+                  buildButton("="),
+                ])
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
